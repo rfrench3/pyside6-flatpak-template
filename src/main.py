06be1d09557cdbf7f6d533f225cf955cc04b8fb4
Robert French <178658682+rfrench3@users.noreply.github.com>
2025-06-23 17:15:57 -0400

    testing new way to get icon

--- src/main.py
+++ src/main.py
@@ -11,36 +11,41 @@ from PySide6.QtUiTools import QUiLoader
 from PySide6.QtCore import QFile
 
 
-ui_main = "./src/mainwindow.ui"
-path_svg = "./io.github.rfrench3.pyside6-flatpak-template.svg"
-path_png = "./io.github.rfrench3.pyside6-flatpak-template.png"
 
 def in_flatpak() -> bool:
     # Checks if the app is running inside a Flatpak sandbox
-    return os.path.exists("/.flatpak-info")
+    return os.environ.get("FLATPAK_SANDBOX_DIR") is not None
 
-def icon_path(path_svg,path_png) -> str:
+def app_icon(path_svg,path_png):
     # tries to load the svg icon, app falls back to png if it fails
     icon = QIcon(path_svg)
     if icon.isNull():
-        path_icon = path_png
+        return QIcon(path_png)
     else:
-        path_icon = path_svg
-    return path_icon
-
+        return icon
+        
 # bundle of ui-launching code
 def launch_window(ui_path:str,window_title:str="WindowTitle"):
     # new_window = launch_window(pathToUI,title)
     loader = QUiLoader()
     ui = QFile(ui_path)
-    ui.open(QFile.ReadOnly)
+    ui.open(QFile.OpenModeFlag.ReadOnly)
     variable_name = loader.load(ui)
     ui.close()
 
     #set window attributes
     variable_name.setWindowTitle(window_title)
-    variable_name.setWindowIcon(QIcon(icon_path(path_svg,path_png)))
+    variable_name.setWindowIcon(icon)
     return variable_name
+
+
+
+
+ui_main = "./src/mainwindow.ui"
+path_svg = "./io.github.rfrench3.pyside6-flatpak-template.svg"
+path_png = "./io.github.rfrench3.pyside6-flatpak-template.png"
+
+# logic for the main window
 
 class MainWindowLogic():
     def __init__(self, window): 
@@ -59,6 +64,8 @@ class MainWindowLogic():
 
 # Logic that loads the main window
 app = QApplication([])
+#icon = app_icon(path_svg,path_png)
+icon = QIcon.fromTheme("io.github.rfrench3.pyside6-flatpak-template")
 
 window_main = launch_window(ui_main,"Basic Window")
 logic = MainWindowLogic(window_main)
